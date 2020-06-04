0c08e4fba430c663ab72698b1b59eb484ca183f6
Praveen Vino <31967668+praveenvino39@users.noreply.github.com>
2020-06-04 20:52:27 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=(BASE_DIR.child('static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
