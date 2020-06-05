dfb83038c84bca2b407ad345f0077c3557798f57
Praveen Vino <31967668+praveenvino39@users.noreply.github.com>
2020-06-06 01:20:41 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -99,6 +99,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 #DJANGO ALL AUTH
 AUTHENTICATION_BACKENDS = [
